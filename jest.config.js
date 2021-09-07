85f6fb80d5d17a372e9421b6226adbf026f6c15c
Igor Souza <igor.souza.webmaster@gmail.com>
2021-09-07 20:31:20 -0300

    feat: add RLT config

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules', '/.next/'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)',
+    '!src/**/*.spec.tsx',
+    '!src/**/_app.tsx',
+    '!src/**/_document.tsx',
+  ],
+};
