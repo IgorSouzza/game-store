a521030f67631cbe1fc485031336325193f4fb56
Igor Souza <igor.souza.webmaster@gmail.com>
2021-09-07 23:11:06 -0300

    fix: ignore stories files

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,9 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   collectCoverage: true,
   collectCoverageFrom: [
-    'src/**/*.ts(x)',
+    'src/**/*.ts(x)?',
     '!src/**/*.spec.tsx',
+    '!src/**/*.stories.tsx',
     '!src/**/_app.tsx',
     '!src/**/_document.tsx',
   ],
