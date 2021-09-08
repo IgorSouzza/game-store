2eea6b0fd49237303f59b499575bdcf1af14d986
Igor Souza <igor.souza.webmaster@gmail.com>
2021-09-07 22:43:58 -0300

    feat: remove knobs and use storybook essentials

--- src/components/Hello/hello.stories.tsx
+++ src/components/Hello/hello.stories.tsx
@@ -1,13 +1,12 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { withKnobs, text } from '@storybook/addon-knobs';
 import Hello from '.';
 
 export default {
   title: 'Hello',
   component: Hello,
-  decorators: [withKnobs],
+  args: {
+    title: 'React Avançado',
+  },
 } as Meta;
 
-export const Basic: Story = () => (
-  <Hello title={text('Title', 'React Avançado')} />
-);
+export const Basic: Story = (args) => <Hello {...args} />;
