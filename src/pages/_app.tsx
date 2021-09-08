2da4fd6fadc34496fc8bf86fdd6b5aca55207e24
Igor Souza <igor.souza.webmaster@gmail.com>
2021-09-07 21:17:07 -0300

    feat: add style components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,23 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { GlobalStyle } from 'styles/global';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with Typescript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default MyApp;
